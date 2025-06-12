1323d7fc0f736a7a861330e26cedaaad52658916
Muyiwa Abidoye <79062706+Muyiiwaa@users.noreply.github.com>
2025-06-12 15:03:17 +0000

    changed port

--- app/main.py
+++ app/main.py
@@ -129,4 +129,4 @@ async def predict_skin_condition(
         )
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload = True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload = True)
